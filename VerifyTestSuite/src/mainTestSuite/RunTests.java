1a2e9b89120870a2aff6e9dfcb1e4b4f912cc9f8
jared <jared@DESKTOP-AAUAOEH>
2023-08-23 11:21:03 -0400

    8/23/2023

---
+++ VerifyTestSuite/src/mainTestSuite/RunTests.java
@@ -0,0 +1,20 @@
+package mainTestSuite;
+
+public class RunTests {
+
+    public static void main(String[] args) {
+    	// Add References to each test file here.
+    	System.out.println("Run Test 1");
+    	System.out.println("Run Test 2");
+    	System.out.println("Run Test 3");
+    	System.out.println("Run Test 4");
+    	System.out.println("Run Test 5");
+    	System.out.println("Run Test 6");
+    	System.out.println("Run Test 7");
+    	System.out.println("Run Test 8");
+    	System.out.println("Run Test 9");
+    	System.out.println("Run Test 10");    	
+    	// Send a final email with the results here.
+    	System.out.println("Send Email");
+    }
+}
